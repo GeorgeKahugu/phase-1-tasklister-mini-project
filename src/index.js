4e4d31f1874ad852c28b743eb619cdfc3ef90549
GeorgeKahugu <gee.kahugu@gmail.com>
2024-03-31 08:57:44 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        alert("Form submitted!");
+    });
 });
+
+
+
+
+
